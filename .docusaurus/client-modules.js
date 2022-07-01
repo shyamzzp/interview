caddfe60dc535d35c5130af32010229bfd9e6410
shyamzzp <shyamsundersuthar009@gmail.com>
2022-07-01 14:20:19 +0530

    Remove the tracked files.

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\src\\css\\custom.css'),
-];
