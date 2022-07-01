8ad04824869918328edba7a6f3f242df1e44c2e5
shyamzzp <shyamsundersuthar009@gmail.com>
2022-07-01 13:18:28 +0530

    Changes for the driver code.

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,7 +1,9 @@
 export default [
+  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\plugin-google-gtag\\lib\\gtag'),
   require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\infima\\dist\\css\\default\\default.css'),
   require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
   require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
   require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
   require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\src\\css\\custom.css'),
+  require('C:\\Users\\shyam\\Desktop\\Self Projects\\interview\\node_modules\\@docusaurus\\plugin-pwa\\lib\\registerSw.js'),
 ];
